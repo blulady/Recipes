3edc8ce9937747db58c13faa38281efb72652fc4
ssanger <blulady@gmail.com>
2023-01-05 21:29:23 -0800

    added xml reporting

--- recipes/settings.py
+++ recipes/settings.py
@@ -141,3 +141,6 @@ LOGIN_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.XMLTestRunner'
+TEST_OUTPUT_DIR = './test_results'
+TEST_OUTPUT_FILE_NAME = 'results.xml'
